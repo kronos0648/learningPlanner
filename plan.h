8d27bd095d7b60427fbc444b28616cb5de67315c
kronos0648 <dkstjdgus15@gmail.com>
2020-12-01 03:40:42 +0900

    add function registerschedule

--- plan.h
+++ plan.h
@@ -10,11 +10,12 @@ typedef struct
 {
 	char **schedule;
 	int row;
-	int column;
+	int col;
 }Schedule;
 
 int getFileAsRead(char*);
-char **getSchedule(int);
+Schedule getSchedule(int fd);
+void printSchedule(Schedule);
 
 
 int getFileAsRead(char* id)
@@ -23,7 +24,7 @@ int getFileAsRead(char* id)
 	strcat(filename,id);
 	strcat(filename,".txt");
 	
-	int f_schedule=fopen(filename,O_RDONLY);
+	int f_schedule=open(filename,O_RDONLY);
 	if(f_schedule==-1)
 	{
 		perror("Can't open file");
@@ -34,30 +35,46 @@ int getFileAsRead(char* id)
 
 Schedule getSchedule(int fd)
 {
-	File *file=fdopen(fd,"r");
+	FILE* file=fdopen(fd,"r");
 	char **schedules;
 	int row=1;
 	int col=0;
+	long position;
+	struct flock lock;
 	
-	**schedules=(char **)malloc(sizeof((char)*row));
-	while(!eof(fp))
+	**schedules=(char **)malloc(sizeof((char*)row));
+	while(!eof(file))
 	{
-		char schedule[100];
-		schedule=(char*)malloc(sizeof((char)*100));
+		lock.l_type=F_RDLCK;
+		lock.l_whence=0;
+		lock.l_start=position;
+		lock.l_len=sizeof(Schedule);
+		if(fcntl(fd,F_SETLKW,&lock)==-1)
+		{
+			printf("lock error\n");
+			exit(-1);
+		}
+		lseek(fd,position,0);
+		char *schedule;
+		int size=sizeof(char*)*100;
+		schedule=(char*)malloc(size);
 		if(fgets(schedule,100,file)==NULL)
 		{
 			perror("can't read File");
-			exit(1);
+			lock.l_type=F_UNLCK;
+			fcntl(fd,F_SETLK,&lock);
 		}
 		
 		if(col<sizeof(schedule))
 		{
 			col=sizeof(schedule);
 		}
-		schedules[row]=malloc(sizeof((char)*col));
+		schedules[row]=malloc(sizeof((char*)col));
 		strcpy(schedules[row],schedule);
 		row++;
 	}
+	lock.l_type=F_UNLCK;
+	fcntl(fd,F_SETLK,&lock);
 	close(fd);
 	Schedule s;
 	s.schedule=schedules;
@@ -75,6 +92,54 @@ void printSchedule(Schedule s)
 		char *content;
 		char scd[s.col];
 		strcpy(scd,s.schedule[i]);
-		char scd
+		char *ptr=strtok(scd,"/");
+		char *seq[100]={"날짜:","과목:","내용:"};
+		int position=0;
+		while(ptr!=NULL)
+		{
+			strcat(seq[position],ptr);
+			printf("%s ",seq[position]);
+			i++;
+			if(i==2) printf("\n");
+		}
 	}
+}
+
+void registerSchedule(char *id,char *date,char *subject,char *content)
+{
+	long position;
+	struct flock lock;
+	lock.l_type=F_WRLCK;
+	lock.l_whence=0;
+	lock.l_start=position;
+	lock.l_len=sizeof(Schedule);
+	
+	
+	char filename[100]="schedule/schedule_@";
+	strcat(filename,id);
+	strcat(filename,".txt");
+	int fd_schedule=open(filename,O_APPEND);
+	if(fcntl(fd_schedule,F_SETLKW,&lock)==-1)
+	{
+		printf("write lock error\n");
+		return;
+	}
+	lseek(fd_schedule,position,0);
+	FILE* f_schedule=fdopen(fd_schedule,"a");
+	
+	int size=sizeof(date)+sizeof(subject)+sizeof(content)+4;
+	char *insert=malloc(size);
+	strcmp(insert,date);
+	strcat(insert,"/");
+	strcat(insert,subject);
+	strcat(insert,"/");
+	strcat(insert,content);
+	
+	fputs(insert,f_schedule);
+	
+	lock.l_type=F_UNLCK;
+	fcntl(fd_schedule,F_SETLK,&lock);
+	close(f_schedule);
+	close(fd_schedule);
+	
 }
